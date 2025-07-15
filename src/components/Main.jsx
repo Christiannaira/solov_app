c4d1d745f66ed03533520ab925ff2dd288bc3c74
Christian <Christianmnaira@gmail.com>
2025-07-15 22:07:59 +0800

    feat: initialized addTask functionality

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,31 @@ import { React, useState } from "react";
 
 const Main = () => {
    const [task, setTask] = useState("");
+   const [taskLists, setTaskLists] = useState([]);
+
+   const addTask = () => {
+
+        if (task != "") {
+            
+            setTaskLists([...taskLists, task]);
+            setTask("");
+
+        }
+
+   }
 
    return (
       <div className="main">
 
          <div className="main_content">
              <h1>Solov Task Tracker</h1>
-            <input type="text" placeholder="Type your new task" />
-            <button type="button">Add Task</button>
+            <input type="text" value={task} placeholder="Type your new task" onChange={(e) => setTask(e.target.value)}/>
+            <button type="button" onClick={addTask}>Add Task</button>
+            <ul>
+                {taskLists.map((task, index) => {
+                    return <li key={index}>{task}</li>
+                })}
+            </ul>
          </div>
       </div>
    );
