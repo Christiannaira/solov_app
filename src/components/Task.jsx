07fd4f3471ca7a5ec7a3ee23ba243279fb813520
Christian <Christianmnaira@gmail.com>
2025-07-17 14:44:16 +0800

    feat: styled the ui for task component

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,35 +1,42 @@
 import { memo, useState } from "react";
 
 const Task = ({ task, id, taskLists, setTaskLists }) => {
-
    const [status, setStatus] = useState(task.completed);
 
    const handleUpdate = () => {
-
-
-      setTaskLists(tasks => 
-      
-        tasks.map((task, index) => 
-        
-          status? (index === id ? {...task, completed: false} : task) : (index === id ? {...task, completed: true} : task)
-        
-        )
-      
-      )
+      setTaskLists((tasks) =>
+         tasks.map((task, index) =>
+            status
+               ? index === id
+                  ? { ...task, completed: false }
+                  : task
+               : index === id
+               ? { ...task, completed: true }
+               : task
+         )
+      );
 
       setStatus(!status);
-
    };
 
    console.log(taskLists);
 
-
    return (
-      <div className={status? "task task_completed " : "task task_not-completed"}>
+      <div
+         className={status ? "task task_completed " : "task task_not-completed"}
+      >
          <h3 className="task_title">{task.task}</h3>
-         <button className={status? "task_btn completed " : "task_btn not-completed"} onClick={handleUpdate}>
-            {status? "Completed" : "Not Completed"}
-         </button>
+         <div className="task_btns">
+            <button className="task_btn">Remove</button>
+            <button
+               className={
+                  status ? "task_btn completed " : "task_btn not-completed"
+               }
+               onClick={handleUpdate}
+            >
+               {status ? "Completed" : "Not Completed"}
+            </button>
+         </div>
       </div>
    );
 };
