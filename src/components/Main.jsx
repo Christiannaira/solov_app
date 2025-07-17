71cdaf6aa7051aaf81d75ad3a697e1a35323b885
Christian <Christianmnaira@gmail.com>
2025-07-17 11:50:10 +0800

    fix: restart from scratch

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,39 +1,10 @@
-import { React, useState } from "react";
-import Task from "./Task";
+
 
 const Main = () => {
-   const [task, setTask] = useState("");
-   const [taskLists, setTaskLists] = useState([]);
-
-   const addTask = () => {
-
-        if (task != "") {
-            
-            setTaskLists([...taskLists, task]);
-            setTask("");
-
-        }
-
-   }
+ 
 
    return (
-      <div className="main">
-
-         <div className="main_content">
-             <h1>Solov Task Tracker</h1>
-            <input className="task_input" type="text" value={task} placeholder="Type your new task" onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                    addTask();
-                }
-            }} onChange={(e) => setTask(e.target.value)}/>
-            <button className="task_btn" type="button" onClick={addTask}>Add Task</button>
-            <ul className="task_list">
-                {taskLists.map((task, index) => {
-                     return <Task key={index} task={task}/>
-                })}
-            </ul>
-         </div>
-      </div>
+    <h1>Hello World</h1>
    );
 };
 
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
-const Task = ({task}) => {
+const Task = ({task, id}) => {
+
+  const handleComplete = (id) => {
+    alert(id);
+  }
+
   return (
     <div className='task'>
       <div className="task_content">
-        {task}
+        <h3 className='task_name'>{task}</h3>
+        <button type='button' className='done_btn' onClick={() => {
+          handleComplete(id);
+        }}>done</button>
       </div>
     </div>
   )
