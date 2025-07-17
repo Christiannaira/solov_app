58a8ac7309f0d8163d7d68c50fcd6b87fa2e1124
Christian <Christianmnaira@gmail.com>
2025-07-17 15:05:34 +0800

    fix: formatting the code + modifying the input value

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,23 +3,25 @@ import { memo } from "react";
 import Task from "./Task";
 
 const Main = () => {
-
    const [task, setTask] = useState("");
    const [taskLists, setTaskLists] = useState([]);
 
    const addTask = () => {
+      if (task != "") {
+         const completed = false;
+         const newTask = { task: task, completed: completed };
 
-        const completed = false;
+         setTaskLists((prev) => {
+            return [...prev, newTask];
+         });
 
-        const newTask = {task: task, completed: completed};
+         setTask("");
+      } else {
+        alert("Your task is empty");
+      }
 
-        setTaskLists((prev) => {
-            return([...prev, newTask]);
-        })
-
-   }
-
-   
+      
+   };
 
    return (
       <>
@@ -27,22 +29,36 @@ const Main = () => {
             <h1 className="main_headline">Solov</h1>
             <div className="main_content">
                <div className="main_content-input">
-                    <input type="text" className="main_input" placeholder="new task" onChange={
-                        (e) => setTask(e.target.value)
-                    } onKeyDown={(e) => {
+                  <input
+                     type="text"
+                     className="main_input"
+                     placeholder="new task"
+                     value={task}
+                     onChange={(e) => setTask(e.target.value)}
+                     onKeyDown={(e) => {
                         if (e.key === "Enter") {
-                            addTask();
+                           addTask();
                         }
-                    }}/>
-                    <button className="main_btn" onClick={addTask}>Add Task</button>
+                     }}
+                  />
+                  <button className="main_btn" onClick={addTask}>
+                     Add Task
+                  </button>
                </div>
 
                <div className="main_content_tasks">
-                    {taskLists.map((task, index) => {
-                        return <Task key={index} task={task} id={index} taskLists={taskLists} setTaskLists={setTaskLists}/>
-                    })}
+                  {taskLists.map((task, index) => {
+                     return (
+                        <Task
+                           key={index}
+                           task={task}
+                           id={index}
+                           taskLists={taskLists}
+                           setTaskLists={setTaskLists}
+                        />
+                     );
+                  })}
                </div>
-
             </div>
          </div>
       </>
